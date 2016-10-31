ae8a3c05f40bb6a755e10c344498b10170b02466
paiba <pedropaivaladeira@hotmail.com>
2016-10-31 00:01:52 -0200

    Com o algoritmo de multiplicação corrigido, apagamos os prints e liberamos a memória

--- sequential/multmatriz.c
+++ sequential/multmatriz.c
@@ -1,42 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float * MultiplicaMatriz(float * matA, float * matB, int tam, int rep)
+float * MultiplicaMatriz(float * matA, float * matB, int tam, int rep) //Função recebe 2 matrizes (matA e matB), tamanho de uma dimensão da matriz quadrada (tam) e quantas repetições da mesma multiplicação serão feitas (rep).//
 {
 	int i,j,k,lugar,r;
+	/*
+	 * i, j e k são contadores para fors do algoritmo de multiplicação
+	 * lugar é o indice da matriz resultado (matriz)
+	 * r é o contador do for de repetições
+	 */
 	float* matriz = (float*)malloc((tam*tam)*(sizeof(float)));
 	float cValue = 0;
 	
 	/* matriz é a matriz resultado que queremos que seja a multiplicação de
 	 * matA por matB nesta ordem.
-	*/
+	 */
 
 	lugar = 0;
 	
-		for(r = 0; r<rep; r++)
-		for(j=0;j<tam;j++)
+		for(r = 0; r<rep; r++)//Este for serve para repetir a multiplicação rep vezes afim de calcular o tempo para comparação com o CUDA
+		for(i=0;i<tam;i++)
 		{
-			for(i=0;i<tam;i++){
+			for(j=0;j<tam;j++){
 				for(k=0;k<tam;k++)
 				{
-					printf("%d %d\n",(j*tam+k),(k*tam+i));
-					cValue+=matA[j*tam+k] * matB[k*tam+i];
+					cValue+=matA[i*tam+k] * matB[k*tam+j];
 				}
 				matriz[lugar]=cValue;
 				lugar++;
 				cValue = 0;
 			}
 		}
-		
-	for(i=0;i<tam*tam;i++)
-	{
-		if(((i % (tam)) ==0) && i>0)
-			printf("\n\t%.2f", matriz[i]);
-		else
-			printf("\t%.2f", matriz[i]);
-	}
-	printf("\n");
-	printf("\n");
 	return matriz;
 }
 
@@ -48,7 +42,7 @@ int main (int argc, char *argv[])
 	
 	float * matA = (float*)malloc(tam*tam*sizeof(float));
 	float * matB = (float*)malloc(tam*tam*sizeof(float));
-	float * matriz = (float*)malloc(tam*tam*sizeof(float));
+	float * matriz;// = (float*)malloc(tam*tam*sizeof(float));
 	
 	for(i=0;i<tam*tam;i++)
 	{
@@ -61,39 +55,10 @@ int main (int argc, char *argv[])
 	{
 		matB[i] = 2;//((float)i+4)/3;
 	}
-	printf("\n");
 	
 	matriz = MultiplicaMatriz(matA, matB, tam, rep);
-	
-	for(i=0;i<tam*tam;i++)
-	{
-		if(((i % (tam)) ==0) && i>0)
-			printf("\n\t%.2f", matB[i]);
-		else
-			printf("\t%.2f", matB[i]);
-	}
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	for(i=0;i<tam*tam;i++)
-	{
-		if(((i % (tam)) ==0) && i>0)
-			printf("\n\t%.2f", matA[i]);
-		else
-			printf("\t%.2f", matA[i]);
-	}
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	for(i=0;i<tam*tam;i++)
-	{
-		if(((i % (tam)) ==0) && i>0)
-			printf("\n\t%.2f", matriz[i]);
-		else
-			printf("\t%.2f", matriz[i]);
-	}
-	printf("\n");
-	printf("\n");
-	printf("\n");
+	free(matA);
+	free(matB);
+	free(matriz);
 	return 0;
 }
