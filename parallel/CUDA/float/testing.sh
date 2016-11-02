4857f481332efc025c42a531ab052965c51e35c8
Matheus Hemerly Risso <matheush.risso@gmail.com>
2016-11-02 20:49:56 -0200

    Script de teste CUDA+Seq; Alguns resultados do script

---
+++ parallel/CUDA/float/testing.sh
@@ -0,0 +1,30 @@
+#Executa as multiplicações {melhor de 3 resultados}
+echo "(512/512) 1 repetição"
+./multiply 512 1
+./multiply 512 1
+./multiply 512 1
+
+echo "(512/512) 100 repetições"
+./multiply 512 100
+./multiply 512 100
+./multiply 512 100
+
+echo "(512/512) 1000 repetições"
+./multiply 512 1000
+./multiply 512 1000
+./multiply 512 1000
+
+echo "(1024/1024) 1 repetição"
+./multiply 1024 1
+./multiply 1024 1
+./multiply 1024 1
+
+echo "(1024/1024) 100 repetições"
+./multiply 1024 100
+./multiply 1024 100
+./multiply 1024 100
+
+echo "(1024/1024) 1000 repetições"
+./multiply 1024 1000
+./multiply 1024 1000
+./multiply 1024 1000
