3ed1f11984af00cf01cc9c9b4fbd8a73a16b9082
Matheus Hemerly Risso <matheush.risso@gmail.com>
2016-11-02 21:14:01 -0200

    Mais testes

---
+++ parallel/CUDA/float/testing2.sh
@@ -0,0 +1,30 @@
+#Executa as multiplicações {melhor de 3 resultados}
+echo "(256/256) 1 repetição"
+./multiply 256 1
+./multiply 256 1
+./multiply 256 1
+
+echo "(256/256) 100 repetições"
+./multiply 256 100
+./multiply 256 100
+./multiply 256 100
+
+echo "(256/256) 1000 repetições"
+./multiply 256 1000
+./multiply 256 1000
+./multiply 256 1000
+
+echo "(128/128) 1 repetição"
+./multiply 128 1
+./multiply 128 1
+./multiply 128 1
+
+echo "(128/128) 100 repetições"
+./multiply 128 100
+./multiply 128 100
+./multiply 128 100
+
+echo "(128/128) 1000 repetições"
+./multiply 128 1000
+./multiply 128 1000
+./multiply 128 1000
